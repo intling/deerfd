80ace4326722ba19a654b3f3cf1482d6ae140af0
intling <address>
2025-08-05 15:30:25 +0800

    修改登录页面、个人信息页面、表格页面的错误

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,158 +1,147 @@
-<script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-import { login } from '../api/user'
-import { useRouter } from 'vue-router'
-import { TokenStore } from '../stores/user'
-import router from '../router'
+<script lang="ts" setup>
+import { ref } from "vue";
+import { login } from "../api/user";
+import { TokenStore } from "../stores/user";
+import router from "../router";
 
-const username = ref('')
-const password = ref('')
-const tokenStore = TokenStore()
-
+const username = ref("");
+const password = ref("");
+const tokenStore = TokenStore();
 
 //登录接口
-const logIn = () => {
-    axios.post('/api/login', {
-    username: username.value,
-    password: password.value
-}).then(res => {
-    console.log(res)
-    tokenStore.setToken(res.token)
-    router.push('/home')
-}).catch(error => {
-    console.error('登录失败:', error)
-    // 提示用户登录失败
-})
-
-const response = login(username.value, password.value)
-console.log(response)
-}
+const logIn = async () => {
+  try {
+    const response = await login({ username: username.value, password: password.value });
+    console.log(response);
+    tokenStore.setToken(response.token);
+    router.push("/home");
+  } catch (error) {
+    console.error("登录失败:", error);
+  }
+};
 </script>
 <template>
-<div class="container" >
+  <div class="container">
     <div class="docker">
-           <div class="left"></div>
-        <div class="login-wrapper">
-            <div class="header">欢迎登录</div>
-            <div class="form-wrapper">
-                <div class="form-group">
-                  <label for="username">用户名</label>
-                  <input type="text" name="username" v-model="username" placeholder="" >
-                </div>
-                <div class="form-group">
-                   <label for="password">密码</label>
-                    <input type="password" name="password" v-model="password" placeholder="" >
-                </div>
-                <a href="#"> 忘记密码?</a>
-                <router-link to="/register" class="zhuce">注册</router-link>
-            </div>
-            <br>
-            <div class="btn" @click="logIn" >登录</div>
+      <div class="left"></div>
+      <div class="login-wrapper">
+        <div class="header">欢迎登录</div>
+        <div class="form-wrapper">
+          <div class="form-group">
+            <label for="username">用户名</label>
+            <input type="text" name="username" v-model="username" placeholder="" />
+          </div>
+          <div class="form-group">
+            <label for="password">密码</label>
+            <input type="password" name="password" v-model="password" placeholder="" />
+          </div>
+          <a href="#"> 忘记密码?</a>
+          <router-link to="/register" class="zhuce">注册</router-link>
         </div>
+        <br />
+        <div class="btn" @click="logIn">登录</div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 <style scoped>
 .container {
-    width: 100%;
-    min-height: 100vh;
-    background-image: url(../image/log1.jpg);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    /* position: fixed; */
-    margin-top: 0;
-    margin-left: 0;
-    
+  width: 100%;
+  min-height: 100vh;
+  background-image: url(../image/log1.jpg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  /* position: fixed; */
+  margin-top: 0;
+  margin-left: 0;
 }
 .docker {
-    width: 30%;
-    height: 60%;
-    border-radius: 15px;
-    display: flex;
-    background: rgba(255, 255, 255, 0.8);
-    margin-left: 50%;
+  width: 30%;
+  height: 60%;
+  border-radius: 15px;
+  display: flex;
+  background: rgba(255, 255, 255, 0.8);
+  margin-left: 50%;
 }
 
 .login-wrapper {
-    flex: 1;
-    height: 55%;
-    padding: 0 50px;
-    position: relative;
-    margin: 0 auto;
-    border-radius: 15px;
-    background: rgba(255, 255, 255, 0.8);
-    /* box-shadow: 0 6px 30px rgba(0, 0, 0, 0.2); */
-    border: 2px solid transparent;
-    background-clip: padding-box;
-    -webkit-mask-image: linear-gradient(0deg, #fff, #fff);
-    mask-image: linear-gradient(0deg, #fff, #fff);
+  flex: 1;
+  height: 55%;
+  padding: 0 50px;
+  position: relative;
+  margin: 0 auto;
+  border-radius: 15px;
+  background: rgba(255, 255, 255, 0.8);
+  /* box-shadow: 0 6px 30px rgba(0, 0, 0, 0.2); */
+  border: 2px solid transparent;
+  background-clip: padding-box;
+  -webkit-mask-image: linear-gradient(0deg, #fff, #fff);
+  mask-image: linear-gradient(0deg, #fff, #fff);
 }
-.login-wrapper:hover{
-    border-image: linear-gradient(to right,#00c6ff,#0072ff) 1;
+.login-wrapper:hover {
+  border-image: linear-gradient(to right, #00c6ff, #0072ff) 1;
 }
-.form-wrapper{
-    display: contents;
-    justify-content: center;
-    align-content: center;
-    gap: 20px;
+.form-wrapper {
+  display: contents;
+  justify-content: center;
+  align-content: center;
+  gap: 20px;
 }
 .header {
-    font-size: 38px;
-    font-weight: bold;
-    text-align: center;
-    line-height: 150px;
-    color: black;
+  font-size: 38px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 150px;
+  color: black;
 }
 .form-group {
-    margin-bottom: 20px;
-    text-align: left;
-
+  margin-bottom: 20px;
+  text-align: left;
 }
 label {
-    display: block;
-    margin-bottom: 8px;
-    font-weight: bold;
-    color: #555;
+  display: block;
+  margin-bottom: 8px;
+  font-weight: bold;
+  color: #555;
 }
-input{
-    width: 97%;
-    height: 40px;
-    border: 2px solid #00c6ff;
-    border-radius: 5px;
-    transition: border-color 0.3s;
-    background-color: rgba(255,255,255,0.3);
+input {
+  width: 97%;
+  height: 40px;
+  border: 2px solid #00c6ff;
+  border-radius: 5px;
+  transition: border-color 0.3s;
+  background-color: rgba(255, 255, 255, 0.3);
 }
-input:focus{
-    border-color: #00f0ff;
-    box-shadow: 0 0 8px rgba(0,198,255,0.6);
-    outline: none;
+input:focus {
+  border-color: #00f0ff;
+  box-shadow: 0 0 8px rgba(0, 198, 255, 0.6);
+  outline: none;
 }
 .btn {
-    padding: 8px;
-    width: 85%;
-    text-align: center;
-    background: linear-gradient(to right,#00c6ff,#0072ff);
-    color: #fff;
-    cursor: pointer;
-    margin: 25px auto;
-    border-radius: 5px;
+  padding: 8px;
+  width: 85%;
+  text-align: center;
+  background: linear-gradient(to right, #00c6ff, #0072ff);
+  color: #fff;
+  cursor: pointer;
+  margin: 25px auto;
+  border-radius: 5px;
 }
-.btn:hover{
-    background: linear-gradient(to right,#00aaff,#005fd9);
-    transform: scale(1.03);
+.btn:hover {
+  background: linear-gradient(to right, #00aaff, #005fd9);
+  transform: scale(1.03);
 }
-.zhuce{
-    float: right;
+.zhuce {
+  float: right;
 }
 .msg {
-    text-align: center;
+  text-align: center;
 }
 a {
-    text-decoration-line: none;
-    color: #abc1ee;
+  text-decoration-line: none;
+  color: #abc1ee;
 }
 </style>
--- src/views/OwnView.vue
+++ src/views/OwnView.vue
@@ -1,3 +1,67 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { Delete, Download, Plus, ZoomIn } from "@element-plus/icons-vue";
+import type { UploadFile } from "element-plus";
+import { save } from "../api/user";
+
+const dialogImageUrl = ref("");
+const dialogVisible = ref(false);
+const disabled = ref(false);
+const uploadRef = ref();
+const isShow = ref(false);
+const isWhite = ref(false);
+const nickname = ref("");
+const password = ref("");
+const email = ref("");
+const gender = ref("");
+const age = ref(0);
+const bio = ref("");
+const isEditing = ref(false);
+
+const handleRemove = (file: UploadFile) => {
+  console.log(file);
+  uploadRef.value?.handleRemove(file);
+};
+
+const handlePictureCardPreview = (file: UploadFile) => {
+  dialogImageUrl.value = file.url!;
+  dialogVisible.value = true;
+};
+
+const handleDownload = (file: UploadFile) => {
+  console.log(file);
+};
+const open = () => {
+  isShow.value = true;
+  isWhite.value = true;
+};
+async function saveMessage() {
+  try {
+    const response = await save({
+      nickname: nickname.value,
+      password: password.value,
+      email: email.value,
+      age: age.value,
+      gender: gender.value,
+      bio: bio.value,
+    });
+    console.log("保存成功", response);
+    alert("保存成功");
+    isEditing.value = false;
+  } catch {
+    alert("保存失败");
+  }
+}
+const cancleButton = () => {
+  isShow.value = false;
+  isWhite.value = false;
+};
+const okButton = () => {
+  isShow.value = false;
+  isWhite.value = false;
+};
+</script>
+
 <template>
   <div class="bigbox">
     <div class="line-content">
@@ -5,69 +69,72 @@
         <label>头像：</label>
       </div>
       <div class="tabar">
-        
         <el-upload ref="uploadRef" action="#" list-type="picture-card" :auto-upload="false">
-        <el-icon><Plus /></el-icon>
-    
-        <template #file="{ file }">
+          <el-icon><Plus /></el-icon>
+
+          <template #file="{ file }">
             <div>
-            <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
-            <span class="el-upload-list__item-actions">
-                <span
-                class="el-upload-list__item-preview"
-                @click="handlePictureCardPreview(file)"
-                >
-                <el-icon><zoom-in /></el-icon>
+              <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+              <span class="el-upload-list__item-actions">
+                <span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
+                  <el-icon><zoom-in /></el-icon>
                 </span>
                 <span
-                v-if="!disabled"
-                class="el-upload-list__item-delete"
-                @click="handleDownload(file)"
+                  v-if="!disabled"
+                  class="el-upload-list__item-delete"
+                  @click="handleDownload(file)"
                 >
-                <el-icon><Download /></el-icon>
+                  <el-icon><Download /></el-icon>
                 </span>
                 <span
-                v-if="!disabled"
-                class="el-upload-list__item-delete"
-                @click="handleRemove(file)"
+                  v-if="!disabled"
+                  class="el-upload-list__item-delete"
+                  @click="handleRemove(file)"
                 >
-                <el-icon><Delete /></el-icon>
+                  <el-icon><Delete /></el-icon>
                 </span>
-            </span>
+              </span>
             </div>
-        </template>
+          </template>
         </el-upload>
-    
+
         <el-dialog v-model="dialogVisible">
-        <img w-full :src="dialogImageUrl" alt="Preview Image" />
+          <img w-full :src="dialogImageUrl" alt="Preview Image" />
         </el-dialog>
       </div>
     </div>
 
-      <div class="quart">
-        <label>昵称：</label>
-        <input type="string" v-model="nickname" :disabled ="!isEditing" />
-      </div>
-      <div class="quart">
-        <label>密码：</label>
-        <input type="password" v-model="password" :disabled ="!isEditing"/>
-      </div>
-      <div class="quart">
-        <label>邮箱：</label>
-        <input type="string" v-model="email":disabled ="!isEditing" />
-      </div>
-      <div class="quart">
-        <label>性别：</label>
-        <input type="string" v-model="gender" :disabled ="!isEditing"/>
-      </div>
-      <div class="quart">
-        <label>年龄：</label>
-        <input type="number" v-model="age" :disabled ="!isEditing"/>
-      </div>
-      <div class="quart">
-        <label class="own-bio">个人简介：</label>
-        <textarea name="" id="biotext" cols="70" rows="7"  v-model="bio" :disabled ="!isEditing"></textarea>
-      </div>
+    <div class="quart">
+      <label>昵称：</label>
+      <input type="string" v-model="nickname" :disabled="!isEditing" />
+    </div>
+    <div class="quart">
+      <label>密码：</label>
+      <input type="password" v-model="password" :disabled="!isEditing" />
+    </div>
+    <div class="quart">
+      <label>邮箱：</label>
+      <input type="string" v-model="email" :disabled="!isEditing" />
+    </div>
+    <div class="quart">
+      <label>性别：</label>
+      <input type="string" v-model="gender" :disabled="!isEditing" />
+    </div>
+    <div class="quart">
+      <label>年龄：</label>
+      <input type="number" v-model="age" :disabled="!isEditing" />
+    </div>
+    <div class="quart">
+      <label class="own-bio">个人简介：</label>
+      <textarea
+        name=""
+        id="biotext"
+        cols="70"
+        rows="7"
+        v-model="bio"
+        :disabled="!isEditing"
+      ></textarea>
+    </div>
     <button class="editMessage" @click="open">编辑</button>
     <button class="saveMessage" @click="saveMessage">保存</button>
   </div>
@@ -75,96 +142,27 @@
   <div class="modal-mask" v-show="isWhite">
     <div class="content" v-show="isShow">
       <h3>个人信息</h3>
-        昵称：<input type="string" v-model="nickname" ><br>
-        密码：<input type="password" v-model="password"><br>
-        邮箱：<input type="string" v-model="email"><br>
-        年龄：<input type="number" v-model="age"><br>
-        性别：<input type="string" v-model="gender"><br>
-        个人简介：<textarea name="" id="bio" cols="50" rows="5"  v-model="bio"></textarea><br>
-        <button class="cancelMessage" @click="cancleButton">cancel</button><button class="okMessage" @click="okButton">OK</button>
+      昵称：<input type="string" v-model="nickname" /><br />
+      密码：<input type="password" v-model="password" /><br />
+      邮箱：<input type="string" v-model="email" /><br />
+      年龄：<input type="number" v-model="age" /><br />
+      性别：<input type="string" v-model="gender" /><br />
+      个人简介：<textarea name="" id="bio" cols="50" rows="5" v-model="bio"></textarea><br />
+      <button class="cancelMessage" @click="cancleButton">cancel</button
+      ><button class="okMessage" @click="okButton">OK</button>
     </div>
   </div>
-  
-  </template>
-  
-<script lang="ts" setup>
-import { ref } from 'vue'
-import { Delete, Download, Plus, ZoomIn } from '@element-plus/icons-vue'
-import type { UploadFile } from 'element-plus'
-import { save } from '../api/user'
- 
-  const dialogImageUrl = ref('')
-  const dialogVisible = ref(false)
-  const disabled = ref(false)
-  const uploadRef = ref()
-  const isShow = ref(false)
-  const isWhite = ref(false)
-  const nickname = ref('')
-  const password = ref('')
-  const email = ref('')
-  const gender = ref('')
-  const age = ref(null)
-  const bio = ref('')
-  const isEditing = ref(false)
+</template>
 
-  const handleRemove = (file: UploadFile) => {
-    console.log(file)
-    uploadRef.value?.handleRemove(file)
-  }
-  
-  const handlePictureCardPreview = (file: UploadFile) => {
-    dialogImageUrl.value = file.url!
-    dialogVisible.value = true
-  }
-  
-  const handleDownload = (file: UploadFile) => {
-    console.log(file)
-  }
-  const submitUpload =() =>{
-    uploadRef.value.submit()
-  }
-  const open = () => {
-    document.querySelector('.editMessage').addEventListener('click',()=>{
-      isShow.value = true
-      isWhite.value = true
-    })
-  }
-  async function saveMessage (){
-    try{
-      const response = await save({
-        nickname:nickname.value,
-        password:password.value,
-        email:email.value,
-        age:age.value,
-        gender:gender.value,
-        bio:bio.value
-      })
-      console.log("保存成功",response)
-      alert('保存成功')
-      isEditing.value = false
-    }catch{
-      alert('保存失败') 
-    }
-    
-  }
-  const cancleButton =() =>{
-    isShow.value = false
-    isWhite.value = false
-  }
-  const okButton = () =>{
-    isShow.value = false
-    isWhite.value = false
-  }
-</script>
-<style scoped> 
-h3{
+<style scoped>
+h3 {
   text-align: center;
 }
-.line-content{
+.line-content {
   display: flex;
   margin-top: 20px;
 }
-.touxiang{
+.touxiang {
   width: 60px;
   font-size: 18px;
   height: 150px;
@@ -175,26 +173,26 @@ h3{
   align-items: center;
 }
 
-.tabar{
-    width: 90%;
-    height: 150px;
-    margin-left: 15px;
+.tabar {
+  width: 90%;
+  height: 150px;
+  margin-left: 15px;
 }
-.modal-mask{
+.modal-mask {
   position: fixed;
   z-index: 999;
   top: 0;
-  left:0;
+  left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0,0,0,0.3);
+  background-color: rgba(0, 0, 0, 0.3);
 }
 
-.content{
+.content {
   width: 500px;
   height: 450px;
   padding: 15px;
-  background-color: rgb(255,255,255);
+  background-color: rgb(255, 255, 255);
   border: 1px solid grey;
   box-shadow: 0 0 5px grey;
   border-radius: 8px;
@@ -202,34 +200,37 @@ h3{
   position: sticky;
   z-index: 1000;
 }
-.quart{
-    width: 40%;
-    height: 18%;
-    margin-top: 20px;
-    margin-left: 20px;
-    font-size: 18px;
+.quart {
+  width: 40%;
+  height: 18%;
+  margin-top: 20px;
+  margin-left: 20px;
+  font-size: 18px;
 }
-.quatr:input{
+.quatr:input {
   height: 32px;
 }
-.own-bio{
+.own-bio {
   width: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.biotext{
+.biotext {
   margin-left: 55px;
 }
-.editMessage,.saveMessage,.cancelMessage,.okMessage{
-    height: 30px;
-    background-color: #007BFF;
-    color: white;
-    border-radius: 3px;
-    margin-top: 20px;
-    border: 1px solid rgb(255,255,255);
+.editMessage,
+.saveMessage,
+.cancelMessage,
+.okMessage {
+  height: 30px;
+  background-color: #007bff;
+  color: white;
+  border-radius: 3px;
+  margin-top: 20px;
+  border: 1px solid rgb(255, 255, 255);
 }
-.saveMessage{
+.saveMessage {
   width: 90px;
   height: 45px;
   font-size: 14px;
@@ -237,32 +238,33 @@ h3{
   font-size: 16px;
   border-radius: 5px;
 }
-.editMessage{
+.editMessage {
   width: 90px;
   height: 45px;
   margin-left: 10%;
   font-size: 16px;
   border-radius: 5px;
 }
-.cancelMessage,.okMessage{
+.cancelMessage,
+.okMessage {
   width: 70px;
   height: 30px;
   margin-left: 23%;
 }
-input{
-    width: 80%;
-    height: 25px;
-    margin-top: 10px;
-    margin-bottom: 8px;
-    border-radius: 5px;
+input {
+  width: 80%;
+  height: 25px;
+  margin-top: 10px;
+  margin-bottom: 8px;
+  border-radius: 5px;
 }
-#bio{
+#bio {
   margin-top: 10px;
 }
-.el-message-box{
-    width: 400px;
-    height: 400px;
-    margin: 0 auto;
-    border: 1px solid grey;
+.el-message-box {
+  width: 400px;
+  height: 400px;
+  margin: 0 auto;
+  border: 1px solid grey;
 }
 </style>
